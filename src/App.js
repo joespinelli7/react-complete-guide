9dd8a8b4089bc1690d56bfcc3d84cdf4634e1cd6
github username <josephspinelli@yahoo.com>
2019-04-28 16:23:18 -0400

    guide

--- src/App.js
+++ src/App.js
@@ -1,27 +1,160 @@
+// import React, { useState } from 'react';
+// import './App.css';
+// import Person from './Person/Person'
+//
+// const app = props => {
+//   const [peopleState, setPeopleState] = useState({
+//     people: [
+//       {name: 'Joe', age: '22'},
+//       {name: 'Adam', age: '23'},
+//       {name: 'Bartholemew', age: '69'}
+//     ]
+//   })
+//
+//   const [otherState, setOtherState] = useState('some other value');
+//
+//   const switchNameHandler = () => {
+//     // console.log('clicked')
+//     setPeopleState({
+//       people: [
+//         {name: 'Joseph', age: '22'},
+//         {name: 'Adam', age: '24'},
+//         {name: 'Bartholemew', age: '69'}
+//       ]
+//     })
+//   }
+//
+//   return (
+//     <div className="App">
+//       <h1>Hi I'm a React App created by a great developer Joe Spinelli!</h1>
+//       <p>I love React!</p>
+//       <button onClick={switchNameHandler}>Switch Name</button>
+//       <Person name={peopleState.people[0].name} age={peopleState.people[0].age}>My profession: Incredible Front End Developer</Person>
+//       <Person name={peopleState.people[1].name} age={peopleState.people[1].age}>My hobbies: saucin'</Person>
+//       <Person name={peopleState.people[2].name} age={peopleState.people[2].age}/>
+//     </div>
+//   );
+//     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m a React App created by a great developer Joe Spinelli!'))
+// }
+//
+// export default app;
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    people: [
+      { id: 'as5', name: 'Joe', age: 22 },
+      { id: 'jk4', name: 'Adam', age: 23 },
+      { id: 'qc8', name: 'Bartholemew', age: 69 }
+    ],
+    otherState: 'some other value',
+    showPeople: false
+  };
+
+  switchNameHandler = (newName, age) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.people[0].name = 'Joseph';
+    this.setState({
+      people: [
+        { name: newName, age: 22 },
+        { name: 'Adam', age: age },
+        { name: 'Bartholemew', age: 69 }
+      ]
+    })
+  }
+
+  nameChangedHandler = (e, id) => {
+    const personIndex = this.state.people.findIndex( person => {
+      return person.id === id;
+    });
+
+    const updatePerson = {
+      ...this.state.people[personIndex]
+    }
+
+    updatePerson.name = e.target.value
+
+    const people = [...this.state.people];
+    people[personIndex] = updatePerson
+
+    this.setState({
+      people: people
+    })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const people = this.state.people.slice();
+    // ^ do the same thing except w/ spread operator is ES6 (most updated version)
+    const people = [...this.state.people];
+    people.splice(personIndex, 1);
+
+    this.setState({
+      people: people
+    })
+  }
+
+  togglePeopleHandler = () => {
+    const doesShow = this.state.showPeople;
+    this.setState({
+      showPeople: !doesShow
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let people = null;
+
+    if (this.state.showPeople) {
+      people = (
+        <div>
+          {this.state.people.map( (person, index)  => {
+            return <Person
+                    name={person.name}
+                    age={person.age}
+                    click={() => this.deletePersonHandler(index)}
+                    key={person.id}
+                    changed={(e) => this.nameChangedHandler(e, person.id)}
+                  />
+          })}
+        </div>
+      )
+
+      style.backgroundColor = 'red'
+    }
+
+    const classes = [];
+    if (this.state.people.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.people.length <= 1) {
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button
+          onClick={this.togglePeopleHandler}
+          style={style}
+        >
+          Toggle People
+        </button>
+        {people}
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
